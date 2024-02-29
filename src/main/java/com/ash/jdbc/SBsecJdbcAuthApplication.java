28b64cde3301a816cd46f3b9458f037261d3b65b
Iamashwini <ashwinishang123@gmail.com>
2024-02-29 18:40:19 +0530

    initial commit

---
+++ src/main/java/com/ash/jdbc/SBsecJdbcAuthApplication.java
@@ -0,0 +1,13 @@
+package com.ash.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBsecJdbcAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SBsecJdbcAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ash/jdbc/SBsecJdbcAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ash.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SBsecJdbcAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
